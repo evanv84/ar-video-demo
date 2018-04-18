f574442537f490d4d275338bf95aa7539e2d256a
ILYA VAKHRIN <vivumexcoriari@ILYAs-MacBook-Pro.local>
2018-04-18 13:04:34 +0400

    video controls fixed

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@ $(document).ready(function() {
 
 	var video = document.getElementById('video');
 	video.pause();
+
+	$('.pause-btn').hide()
 	
 	$(document).on('click', '.play-btn', function(event) {
 		event.preventDefault();
 		video.play();
-		$('.play-btn').hide();
+		$('.play-btn').hide(400);
+		$('.pause-btn').show(400);
+	});
+
+	$(document).on('click', '.pause-btn', function(event) {
+		event.preventDefault();
+		video.pause();
+		$('.play-btn').show(400);
+		$('.pause-btn').hide(400);
 	});
 
 });
